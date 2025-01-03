1b7d39fdc2fa7a28a2e057bad2b8a40e3df7e59e
Ashwin Shrivastav <103900575+ASH-will-WIN@users.noreply.github.com>
2025-01-03 12:18:26 -0500

    Updated with AccelStepper and PurePursuit
    This still uses pure pursuit but with Accel Stepper

--- RobotTour24/RobotTour24.ino
+++ RobotTour24/RobotTour24.ino
@@ -1,106 +1,228 @@
-//Define stepper motor connections and steps per revolution:
-#define dirPin 2
-#define stepPin 3
-#define stepsPerRevolution 3200  // 200 * 16 for 1/16 microstepping
+#include <Math.h>
+#include <AccelStepper.h>
 
-#define dirPin2 4
-#define stepPin2 5
 
-// Microstepping control pins
+// Left is pin1 side from Northview code
+// Right is pin2 side from Northview code
+#define DIR_PIN_LEFT         2      
+#define STEP_PIN_LEFT         3      
+
+#define DIR_PIN_RIGHT         4
+#define STEP_PIN_RIGHT        5
+
+
+// For 1/16th microstepping
 #define MS1 6
 #define MS2 7
 #define MS3 8
 
-#define FORWARD_1 HIGH
-#define FORWARD_2 LOW
-#define REVERSE_1 LOW
-#define REVERSE_2 HIGH
 
-float wheelDiameter = 4.0132 * 2;
-float circumference = M_PI * wheelDiameter;
-double wheelbase = 15.875;
+
+
+AccelStepper leftMotor(AccelStepper::DRIVER, STEP_PIN_LEFT, DIR_PIN_LEFT);
+AccelStepper rightMotor(AccelStepper::DRIVER, STEP_PIN_RIGHT, DIR_PIN_RIGHT);
+
+long previousTime = millis();
+
+struct Point {
+  double x;  // x-coordinate
+  double y;  // y-coordinate
+};
+
+double lookaheadDistance = 10;
+double baseSpeed = 500;
+double maxSpeed = 2000;
+double scaleFactor = 1;
+
+double currentLeftSteps = 0;
+double currentRightSteps = 0;
+double previousLeftSteps = 0;
+double previousRightSteps = 0;
+
+
+double rightSpeed;
+double leftSpeed;
+
+
+
+double WHEELBASE = 15.875;
+double WHEEL_RADIUS = 4.0132;
+double WHEEL_CIRCUMFERENCE = 2 * PI * WHEEL_RADIUS;
+double STEPS_PER_REV = 200 * 16;  // 16 is the 1/16 microstepping
+double dStep = WHEEL_CIRCUMFERENCE/STEPS_PER_REV;
+double totalDistance = 0;
+
+
+double initialHeading = 0;
+double currentHeading = initialHeading;
+Point initialPos = {0, 0};
+Point currentPos = {0, 0};
+Point desiredPositions[5] = {{0, 0},
+                                 {0, 10},
+                                 {0, 20},
+                                 {0, 30},
+                                 {0, 40}};
+
+
+
+
 
 void setup() {
-  // Declare pins as output:
-  pinMode(stepPin, OUTPUT);
-  pinMode(dirPin, OUTPUT);
-  pinMode(stepPin2, OUTPUT);
-  pinMode(dirPin2, OUTPUT);
+  // Initialize hardware serial for debugging
+  Serial.begin(115200);
+
+
+  // Set pin modes for both motors
+  pinMode(DIR_PIN_LEFT, OUTPUT);
+  pinMode(DIR_PIN_RIGHT, OUTPUT);
+
+  pinMode(STEP_PIN_LEFT, OUTPUT);
+  pinMode(STEP_PIN_RIGHT, OUTPUT);
+
+
 
   // Microstepping pins:
   pinMode(MS1, OUTPUT);
   pinMode(MS2, OUTPUT);
   pinMode(MS3, OUTPUT);
-
   // Set microstepping to 1/16
   digitalWrite(MS1, HIGH);
   digitalWrite(MS2, HIGH);
   digitalWrite(MS3, HIGH);
 
-  Serial.begin(115200);
-  Serial.println("Code is running with 1/16 microstepping");
-  delay(10000);
-
-  // Test movement
-  moveForward(25, 5000);
-  turnRight(15, 5000);
-  delay(1000);
-  moveForward(150, 5000);
-  delay(1000);
-  turnLeft(15, 5000);
-  delay(1000);
-  moveForward(200, 5000);
-  delay(1000);
-  turnLeft(15, 5000);
-  delay(1000);
-  moveForward(150, 5000);
-  turnLeft(15, 5000);
-  moveForward(150, 5000);
-  turnLeft(15, 5000);
-  moveForward(150, 5000);
+  rightMotor.setSpeed(50);
+  leftMotor.setSpeed(50);
+  rightMotor.setAcceleration(50);
+  leftMotor.setAcceleration(50);
+  rightMotor.moveTo(500);
+  leftMotor.moveTo(500);
+  rightMotor.run();
+  leftMotor.run();
 }
+
 
 void loop() {
-  // Empty loop for now
-}
 
-float distanceToSteps(int cm) {
-  float revolutions = cm / circumference;
-  return revolutions * stepsPerRevolution;
-}
+  updateValues();
 
-void move(int distance, int speed) {
-  for (int i = 0; i < distanceToSteps(distance); i++) {
-    // Step Motor 1
-    digitalWrite(stepPin, HIGH);
-    digitalWrite(stepPin2, HIGH);
-    delayMicroseconds(speed / 16); // Adjust speed for microstepping
-    digitalWrite(stepPin, LOW);
-    digitalWrite(stepPin2, LOW);
-    delayMicroseconds(speed / 16);
+  adjustMotorSpeeds(calculateCurvature(findLookaheadPoint()));
+
+  leftMotor.setSpeed(constrain(leftSpeed, 0, maxSpeed));
+  rightMotor.setSpeed(constrain(rightSpeed, 0, maxSpeed));
+
+
+  if(isPathComplete(desiredPositions[4], 3)) {
+    leftMotor.stop();
+    rightMotor.stop();
+
+    while(true){}
+  }
+
+  leftMotor.run();
+  rightMotor.run();
+
+  // Serial.print("LStepsTotal: "); Serial.print(leftMotor.currentPosition()); 
+  // Serial.print("    RStepsTotal: "); Serial.print(rightMotor.currentPosition());
+  // Serial.print("    Heading: "); Serial.print(currentHeading);
+  // Serial.print("    XPos: "); Serial.print(currentPos.x); Serial.print("  YPos: "); Serial.println(currentPos.y);
+  // Serial.println(" ");
+
+  if (millis() - previousTime >= 100) {
+        Serial.print("LStepsTotal: "); Serial.print(leftMotor.currentPosition());
+        Serial.print("    RStepsTotal: "); Serial.print(rightMotor.currentPosition());
+        Serial.print("    Heading: "); Serial.print(currentHeading);
+        Serial.print("    XPos: "); Serial.print(currentPos.x); Serial.print("  YPos: "); Serial.println(currentPos.y);
+        previousTime = millis();
   }
 }
 
-void moveForward(int distance, int speed) {
-  digitalWrite(dirPin, FORWARD_1);
-  digitalWrite(dirPin2, FORWARD_2);
-  move(distance, speed);
+
+
+void updateHeading() {
+  double deltaSteps = currentLeftSteps - currentRightSteps;
+  double deltaHeading = (deltaSteps * dStep)/WHEELBASE;
+  currentHeading += deltaHeading;
+
+  currentHeading = fmod(currentHeading, 2 * PI);
+  if (currentHeading < 0) {
+    currentHeading += 2 * PI;
+  }
 }
 
-void moveBackward(int distance, int speed) {
-  digitalWrite(dirPin, REVERSE_1);
-  digitalWrite(dirPin2, REVERSE_2);
-  move(distance, speed);
+
+void updateDistanceAndPos() {
+  double avgDeltaSteps = ((currentLeftSteps - previousLeftSteps) + (currentRightSteps - previousRightSteps))/2;
+  double deltaDistance = avgDeltaSteps * dStep;
+  
+  totalDistance += deltaDistance;
+  currentPos.x += deltaDistance * cos(currentHeading);
+  currentPos.y += deltaDistance * sin(currentHeading);
 }
 
-void turnRight(int distance, int speed) {
-  digitalWrite(dirPin, HIGH);
-  digitalWrite(dirPin2, HIGH);
-  move(distance, speed);
+void updateStepCounts(){
+  previousLeftSteps = currentLeftSteps;
+  previousRightSteps = currentRightSteps;
+
+  currentLeftSteps = leftMotor.currentPosition();
+  currentRightSteps = rightMotor.currentPosition();
 }
 
-void turnLeft(int distance, int speed) {
-  digitalWrite(dirPin, LOW);
-  digitalWrite(dirPin2, LOW);
-  move(distance, speed);
+void updateValues(){
+  updateStepCounts();
+  updateHeading();
+  updateDistanceAndPos();
+}
+
+Point findLookaheadPoint(){
+  for (int i = 0; i < (sizeof(desiredPositions)/sizeof(desiredPositions[0])) - 1; i++){
+    double dx = desiredPositions[i + 1].x - currentPos.x;
+    double dy = desiredPositions[i + 1].y - currentPos.y;
+    double distance = sqrt(dx*dx + dy*dy);
+
+    if (distance >= lookaheadDistance) {
+      return desiredPositions[i + 1];
+    }
+  }
+
+  return desiredPositions[(sizeof(desiredPositions)/sizeof(desiredPositions[0])) - 1];
+}
+
+
+double calculateCurvature(Point lookaheadPoint) {
+  double dx = lookaheadPoint.x - currentPos.x;
+  double dy = lookaheadPoint.y - currentPos.y;
+  double angleToLookahead = atan2(dy, dx);
+  double alpha = angleToLookahead - currentHeading;
+
+  double distance = sqrt(dx*dx + dy*dy);
+  double curve = (2 * sin(alpha))/distance;
+  return curve;
+}
+
+
+void adjustMotorSpeeds(double k) {
+  double leftMSpeed = baseSpeed * (1 - k * WHEELBASE/2);
+  double rightMSpeed = baseSpeed * (1 + k * WHEELBASE/2);
+  
+  leftSpeed = constrain(leftMSpeed, 0, maxSpeed);
+  rightSpeed = constrain(rightMSpeed, 0, maxSpeed);
+}
+
+
+
+bool isPathComplete(Point targetPos, double threshold) {
+  double dx = targetPos.x - currentPos.x;
+  double dy = targetPos.y - currentPos.y;
+  double distance = sqrt(dx * dx + dy * dy);
+  return distance <= threshold;
+}
+
+
+double millisToSeconds(double ms) {
+  return ms/1000.0000;
+}
+
+
+double secondsToMillis(double s) {
+  return s * 1000.0000;
 }
